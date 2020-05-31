a924775131645a0d0e8a16864af4926427755596
Damian Bemben <bembendamian@yahoo.co.uk>
2020-05-31 19:01:53 +0100

    Large styling update

--- ClimateMonitorV2/public/javascripts/new_mapping.js
+++ ClimateMonitorV2/public/javascripts/new_mapping.js
@@ -85,10 +85,9 @@ $(document).ready(() => {
             console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
         });
     });
+    human_display(1000, 0, "#danger_level")
+    human_display(700, 300, "#danger_level2")
 
-
-
-    human_display(700, 300, "#danger_level")
     var scatterChartPM2 = new Chart(pm2Chart, {
         type: 'scatter',
         data: {
@@ -160,13 +159,13 @@ $(document).ready(() => {
         data: {
             labels: ["pm10", "pm2.5"],
             datasets: [
-                { label: 'Within guidelines', data: [20, 10], backgroundColor: [safe, safe], borderWidth: 1 },
-                { label: '3% Increase In LTM', data: [30, 15], backgroundColor: [light, light,], borderWidth: 1 },
-                { label: '9% Increase In LTM', data: [50, 25], backgroundColor: [medium, medium], borderWidth: 1 },
-                { label: '15% Increase in LTM', data: [75, 37.5], backgroundColor: [high, high], borderWidth: 1 },
-                { label: '1.2% Increase In STM, 15% Increase in LTM', data: [100, 50], backgroundColor: [veryhigh, veryhigh], borderWidth: 1 },
-                { label: '2.5% Increase In STM, 15% Increase in LTM', data: [150, 75], backgroundColor: [danger, danger], borderWidth: 1 },
-                { label: '5% Increase In STM, 15% Increase in LTM', data: [175, 175], backgroundColor: [bigdanger, bigdanger], borderWidth: 1 }]
+                { label: 'Within guidelines', data: [20, 10], backgroundColor: [safe, safe], borderWidth: 0 },
+                { label: '3% Increase In LTM', data: [30, 15], backgroundColor: [light, light,], borderWidth: 0 },
+                { label: '9% Increase In LTM', data: [50, 25], backgroundColor: [medium, medium], borderWidth: 0 },
+                { label: '15% Increase in LTM', data: [75, 37.5], backgroundColor: [high, high], borderWidth: 0 },
+                { label: '1.2% Increase In STM, 15% Increase in LTM', data: [100, 50], backgroundColor: [veryhigh, veryhigh], borderWidth: 0 },
+                { label: '2.5% Increase In STM, 15% Increase in LTM', data: [150, 75], backgroundColor: [danger, danger], borderWidth: 0 },
+                { label: '5% Increase In STM, 15% Increase in LTM', data: [175, 175], backgroundColor: [bigdanger, bigdanger], borderWidth: 0 }]
         },
         options: {
             title: { display: true, text: "Effects of PM10 and PM2.5 on Long and Short Term Mortality (LTM, STM)" },
@@ -271,21 +270,24 @@ $(document).ready(() => {
         var pm10Color = []
         scatterChartPM10.data.datasets = []
         scatterChartPM2.data.datasets = []
-        for (i = 1; i < data.length; i++) {
-            date = new Date(data[i]['timestamp'])
-            pm2 = { x: date, y: parseFloat(data[i]['P2']) }
-            pm10 = { x: date, y: parseFloat(data[i]['P1']) }
-            pm2Color.push('green')
-            pm10Color.push('red')
-            pm2Data.push(pm2)
-            pm10Data.push(pm10)
+        if (data.length == 0) {
+            alert("Sorry, the data wasn't found!")
+        } else {
+            for (i = 1; i < data.length; i++) {
+                date = new Date(data[i]['timestamp'])
+                pm2 = { x: date, y: parseFloat(data[i]['P2']) }
+                pm10 = { x: date, y: parseFloat(data[i]['P1']) }
+                pm2Color.push('green')
+                pm10Color.push('red')
+                pm2Data.push(pm2)
+                pm10Data.push(pm10)
+            }
+            console.log(scatterChartPM10.data)
+            scatterChartPM10.data.datasets.push({ label: "Pm10 Data", data: pm10Data, backgroundColor: 'red' })
+            scatterChartPM2.data.datasets.push({ label: "Pm2 Data", data: pm2Data, backgroundColor: 'blue' })
+            scatterChartPM10.update()
+            scatterChartPM2.update()
         }
-        console.log(scatterChartPM10.data)
-        scatterChartPM10.data.datasets.push({ label: "Pm10 Data", data: pm10Data, backgroundColor: 'red' })
-        scatterChartPM2.data.datasets.push({label: "Pm2 Data", data:pm2Data, backgroundColor: 'blue'})
-        scatterChartPM10.update()
-        scatterChartPM2.update()
-        
 
     }
     var json = $.getJSON('http://api.luftdaten.info/static/v2/data.24h.json', function (data) {
@@ -306,7 +308,7 @@ $(document).ready(() => {
                 color: 'black',
                 fillColor: color,
                 fillOpacity: 0.8,
-                radius: 150,
+                radius: 100,
                 p10: [items[i][3]],
                 p2: [items[i][4]],
                 sensor_id: [items[i][5]],
