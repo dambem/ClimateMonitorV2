267a24c3a2428d75d1d2300e551e6a4eac5a0b72
Damian Bemben <bembendamian@yahoo.co.uk>
2020-07-15 18:01:57 +0100

    Sidebar styling improvements

--- ClimateMonitorV2/public/javascripts/config.js
+++ ClimateMonitorV2/public/javascripts/config.js
@@ -1,6 +1,6 @@
 // PLEASE ASK DAMIAN FOR ANY KEYS YOU DON'T HAVE
 var config = {
-    MAP_KEY: "",
+    MAP_KEY: "pk.eyJ1IjoiZGFtYmVtIiwiYSI6ImNrYjJnbjQ1djBvbTkzMmxvMTFpZ2lvMWEifQ.jwAs_GyT8Q1Rhu8NAPcJYA",
     WEATHER_COMPANY_KEY: "",
     GITHUB_API_TOKEN: ""
 }
