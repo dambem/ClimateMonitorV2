d5a35d465fecaaa51a16c4601b27bae26f0e23f5
Damian Bemben <bembendamian@yahoo.co.uk>
2020-07-15 21:04:47 +0100

    Design and bug fixing

--- ClimateMonitorV2/public/javascripts/config.js
+++ ClimateMonitorV2/public/javascripts/config.js
@@ -1,6 +1,6 @@
 // PLEASE ASK DAMIAN FOR ANY KEYS YOU DON'T HAVE
 var config = {
-    MAP_KEY: "",
-    WEATHER_COMPANY_KEY: "",
-    GITHUB_API_TOKEN: ""
+    MAP_KEY: "blah",
+    WEATHER_COMPANY_KEY: "blah",
+    GITHUB_API_TOKEN: "blah"
 }
