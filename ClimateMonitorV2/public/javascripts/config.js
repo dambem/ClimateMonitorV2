8d2886d08c2ef30a45c29e191767befabe482ee5
Damian Bemben <bembendamian@yahoo.co.uk>
2020-07-16 22:54:09 +0100

    Re-fixing the CSS as some was missing
    also made the sidebar slightly smaller (i'm sorry i'll stop now)

--- ClimateMonitorV2/public/javascripts/config.js
+++ ClimateMonitorV2/public/javascripts/config.js
@@ -1,6 +1,6 @@
 // PLEASE ASK DAMIAN FOR ANY KEYS YOU DON'T HAVE
 var config = {
-    MAP_KEY: "blah",
+    MAP_KEY: "pk.eyJ1IjoiZGFtYmVtIiwiYSI6ImNrYjJnbjQ1djBvbTkzMmxvMTFpZ2lvMWEifQ.jwAs_GyT8Q1Rhu8NAPcJYA",
     WEATHER_COMPANY_KEY: "blah",
     GITHUB_API_TOKEN: "blah"
 }
