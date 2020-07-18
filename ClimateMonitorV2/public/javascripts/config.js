1a607877daedcbf378ccc07f20f6149c62634774
Damian Bemben <bembendamian@yahoo.co.uk>
2020-07-18 12:38:33 +0100

    Delete config.js

--- ClimateMonitorV2/public/javascripts/config.js
+++
@@ -1,6 +0,0 @@
-// PLEASE ASK DAMIAN FOR ANY KEYS YOU DON'T HAVE
-var config = {
-    MAP_KEY: "pk.eyJ1IjoiZGFtYmVtIiwiYSI6ImNrYjJnbjQ1djBvbTkzMmxvMTFpZ2lvMWEifQ.jwAs_GyT8Q1Rhu8NAPcJYA",
-    WEATHER_COMPANY_KEY: "blah",
-    GITHUB_API_TOKEN: "blah"
-}
