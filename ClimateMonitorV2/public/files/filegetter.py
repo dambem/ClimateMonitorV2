f7ec9af7d8fed0c5a4c752a57e37ed8df3b0e21d
Morgan Davies <morgan.davies@ibm.com>
2020-07-30 17:30:37 +0100

    Initial python script checker
    Runs a github check that downloads a 3.x version of python, installs dependancies and runs flake8 to lint the files. I also added an npm script (`pylint`) so you can run the flake8 command locally from anywhere in the `ClimateMonitorV2/ClimateMonitorV2` directory. All python files have been updated to account for this change so only new contributions will break flake8.

--- ClimateMonitorV2/public/files/filegetter.py
+++ ClimateMonitorV2/public/files/filegetter.py
@@ -11,9 +11,11 @@ data = urllib.request.urlopen(url).read().decode()
 # parse json object
 obj = json.loads(data)
 print("Done")
+
 # output some object attributes
 print(obj[0]["location"])
 list_of_sensors = []
+
 for n in obj:
     try:
         latitude = float(n["location"]["latitude"])
@@ -21,47 +23,49 @@ for n in obj:
         if (longitude > -1.58 and longitude < -1.34 and latitude <= 53.468 and latitude >= 53.29):
             if (n["sensor"]["sensor_type"]["name"] == "SDS011"):
                 list_of_sensors.append(n["sensor"]["id"])
-    except ValueError: 
+    except ValueError:
         continue
+
+
 print("Found all sensors in sheffield")
 print(list_of_sensors)
 print(len(list_of_sensors))
 print("Beginning to take dates, first one:")
 amount_of_days = 365
-# Print iterations progress
 
-# Initial call to print 0% progress
-
-
-progresscounter = amount_of_days*len(list_of_sensors)
-
+progresscounter = amount_of_days * len(list_of_sensors)
 
 currprog = 0
 list_of_sensors = [31706]
 
 for n in range(amount_of_days):
     for sensor in list_of_sensors:
-        previous_date = date.today() - timedelta(days=n+2)
+        previous_date = date.today() - timedelta(days=n + 2)
         year = str(previous_date.year)
+
         if previous_date.month < 10:
-            month = "0"+str(previous_date.month)
+            month = "0" + str(previous_date.month)
         else:
             month = str(previous_date.month)
         if previous_date.day < 10:
-            day = "0"+str(previous_date.day)
+            day = "0" + str(previous_date.day)
         else:
             day = str(previous_date.day)
+        
         date_at = year + "-" + month + "-" + day
-        folder = date_at+"/sds011/"
-        print ("Creating: " + date_at + folder + "Sensor Number: " + str(sensor))
-        url = "http://archive.sensor.community/"+date_at+"/"+date_at+"_"+"sds011_sensor_"+str(sensor)+".csv"
+        folder = date_at + "/sds011/"
+        print("Creating: " + date_at + folder + "Sensor Number: " + str(sensor))
+        url = "http://archive.sensor.community/" + date_at + "/" + date_at + "_" + "sds011_sensor_" + str(sensor) + ".csv"
         print(url)
+
         if os.path.exists(folder):
             print("Folder already exists, so not making a new one")
         else:
             os.makedirs(folder)
-        file = str(sensor)+".csv"
-        full_file_dir = folder+file
+        
+        file = str(sensor) + ".csv"
+        full_file_dir = folder + file
+
         if os.path.exists(full_file_dir):
             print("File Exists, so Skipping!")
         else:
@@ -70,9 +74,8 @@ for n in range(amount_of_days):
             except urllib.error.HTTPError:
                 print("HTTP error, skipping")
                 time.sleep(1)
+        
         currprog += 1
         print("Current Download Progress:")
-        print(currprog/progresscounter*100)
+        print(currprog / progresscounter * 100)
         time.sleep(0.1)
-        
-        
--- ClimateMonitorV2/public/files/graphmaker.py
+++ ClimateMonitorV2/public/files/graphmaker.py
@@ -1,42 +1,40 @@
-# ugly ugly code
-import json
-import urllib.request
 import csv
-import os
 from datetime import date, timedelta
 import matplotlib.pyplot as plt
-import matplotlib.dates as md
-import dateutil
 import numpy as np
 
 def date_getter(chosen_date, offset, n):
-    previous_date = chosen_date - timedelta(days=n+offset)
+    previous_date = chosen_date - timedelta(days=n + offset)
     year = str(previous_date.year)
     if previous_date.month < 10:
-        month = "0"+str(previous_date.month)
+        month = "0" + str(previous_date.month)
     else:
         month = str(previous_date.month)
     if previous_date.day < 10:
-        day = "0"+str(previous_date.day)
+        day = "0" + str(previous_date.day)
     else:
         day = str(previous_date.day)
     date_at = year + "-" + month + "-" + day
     return date_at
 
-def csv_parser():
-    dates = []
 
-    pm2_list = []
-    days = 110
-    daily_averages = []
-    daily_dates = []
-    for n in range(days):
-     date_chosen = date_getter(date.today(), 4, n)
-     pm10_list = []
-     with open(date_chosen+'/sds011/31706.csv', 'rt') as csvfile:
+dates = []
+
+pm2_list = []
+days = 110
+daily_averages = []
+daily_dates = []
+
+for n in range(days):
+    date_chosen = date_getter(date.today(), 4, n)
+    pm10_list = []
+
+    with open(date_chosen + '/sds011/31706.csv', 'rt') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=";", quotechar="|")
         rowcount = 0
+
         for row in spamreader:
+            
             if rowcount == 0:
                 print("skipping")
             else:
@@ -47,14 +45,12 @@ def csv_parser():
                 dates.append(hours)
                 pm10_list.append(float(pm10))
                 pm2_list.append(float(pm2))
-            rowcount+=1
+            
+            rowcount += 1
+    
         daily_dates.append(date_chosen)
         print(np.average(pm10_list))
-        #daily_averages.append(np.average(pm10_list))
         daily_averages.append(np.average(pm2_list))
-    real_dates = dates
-    
-    plt.plot(daily_dates, daily_averages)
-    plt.show()
 
-csv_parser()
+plt.plot(daily_dates, daily_averages)
+plt.show()
